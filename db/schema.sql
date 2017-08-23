7c3c393def8bdd935565bce2d3fc1afd9c1897bb
Heather Sosa <heatherlsosa@gmail.com>
2017-08-23 09:33:27 -0600

    update structure and package.json with sequelize ORM

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,5 @@
 ### Schema
 
-CREATE DATABASE cat_db;
-USE cat_db;
+CREATE DATABASE recipes_db;
+USE recipes_db;
 
-CREATE TABLE cats
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
-);
