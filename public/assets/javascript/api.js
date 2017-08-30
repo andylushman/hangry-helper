d076e34c63d7bbbd69d6313f2b6462b8c92504f7
Andy Lushman <andylushman@gmail.com>
2017-08-29 21:24:13 -0600

    changed new card

--- public/assets/javascript/api.js
+++ public/assets/javascript/api.js
@@ -33,34 +33,29 @@ $("#search-btn").on("click", function(){
 
 			var newCard = $( "<div class='col-sm-3'>" )
 
+			newCard.html("<div class='card text-center'> <img class= 'img-fluid' id='image' src='" + results.recipe.image + "' alt = 'card image cap'> <div class='card-body text-center'>" + "<h4 class = 'card-title' id='rtitle'><strong>" + results.recipe.label + "</strong></h4> <h6 class= 'text-danger text-muted'><u>Ingredient List</u></h6> <p class='card-text text-primary text-center' id = 'ingredients'> " + ing + " </p> <p><small class='text-muted' id='source'> Courtesy of: " + results.recipe.source + "</p></small> <a class='btn btn-primary' id='url' href='" +  results.recipe.url + " target='_blank'>Recipe!</a><a class='btn btn-success' href=" +  results.recipe.shareAs + "' target='_blank'>Nutrients!</a><br><br><a href='/favs' id='save-btn' class='btn btn-info'>Save Recipe!</a></div> </div></div>");
+            $(".results").append(newCard);
 
-			newCard.html("<div class='card text-center'> <img class= 'img-fluid id='image' src=" + results.recipe.image + " alt = 'card image cap'> <div class='card-body text-center>" + "<h4 class = 'card-title' id='rtitle'><strong>" + results.recipe.label + "</strong></h4> <h6 class= text-danger text-muted><u>Ingredient List</u></h6> <p class='card-text text-primary text-center' id = 'ingredients'> " + ing + " </p> <p><small class='text-muted' id='source'> Courtesy of: " + results.recipe.source + "</p></small> <a class='btn btn-primary' id='url' href=" +  results.recipe.url + " target='_blank'>Recipe!</a><a class='btn btn-success' href=" +  results.recipe.shareAs + " target='_blank'>Nutrients!</a><br><br><a href='/fav' id='save-btn' class='btn btn-info'>Save Recipe!</a></div> </div></div>");
+            console.log(response.hits[i]);
 
-
-			$(".results").append(newCard);
-
-			console.log(response.hits[i]);
-
-		};
-
-	})
+        };
+    })
 });
-
   $(document).on("click", "#save-btn", function(event) {
-	event.preventDefault();
+    event.preventDefault();
+    var parent = $(this).parent().parent();
     // This function inserts a new todo into our database and then updates the view
-   	var newRecipe = {
-  	  title: $("#rtitle").text(),
-      image: $("#image").attr('src'),
-      source: $("#source").text(),
-      url: $("#url").attr('src'),
-      ingredients: $("#ingredients").text()
+    var newRecipe = {
+      title: parent.find("#rtitle").text(),
+      image: parent.find('img').attr('src'),
+      source: parent.find("#source").text(),
+      url: parent.find('#url').attr('src'),
+      ingredients: parent.find("#ingredients").text()
     }
-
    $.post("/favs/new", newRecipe)
     // On success, run the following code
     .done(function(data) {
       // Log the data we found
-      console.log(data);
+      // console.log(data);
     });
 });
